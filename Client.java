d186e61a840756405e95bc3fee765ba6b40b9908
ivarlund <49944364+ivarlund@users.noreply.github.com>
2020-03-03 14:52:03 +0100

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,104 @@
+
+/**
+ * Client of this chat implementation.
+ * Connects to a server via a Socket and
+ * can send and receive messages from the server.
+ * 
+ * @author Ivar Lund
+ * ivlu1468
+ * ivarnilslund@gmail.com
+ * 
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Main class. Needs a server to operate.
+ * 
+ * @author Ivar Lund
+ *
+ */
+public class Client {
+
+	private Socket socket;
+	private PrintWriter output;
+
+	private Scanner scan = new Scanner(System.in);
+	private Listener listener;
+
+	/**
+	 * Class constructor. Takes two parameters definable by user at startup but may
+	 * be left out to use predetermined values.
+	 * 
+	 * @param hostName the host address for socket.
+	 * @param portNr   the port number for socket.
+	 */
+	public Client(String hostName, int portNr) {
+		setupIO(hostName, portNr);
+		sendMsg();
+	}
+
+	/**
+	 * Initiates I/O, socket and listener thread.
+	 * 
+	 * @param hostName passed from class constructor
+	 * @param portNr   passed from class constructor
+	 */
+	private void setupIO(String hostName, int portNr) {
+		try {
+			socket = new Socket(hostName, portNr);
+			output = new PrintWriter(socket.getOutputStream(), true);
+		} catch (IOException e) {
+			System.out.println("Connection setup failed... \nterminated");
+			System.exit(1);
+		}
+		listener = new Listener(socket);
+		listener.start();
+	}
+
+	/**
+	 * Worker method of client. Runs continuously to output messages. 'quit' command
+	 * to terminate program.
+	 */
+	private void sendMsg() {
+		if (socket.isConnected()) {
+			System.out.println("Output ready.");
+			while (true) {
+				String msg = scan.nextLine();
+				if (msg.equalsIgnoreCase("quit"))
+					killIO();
+				output.println(msg);
+			}
+		}
+	}
+
+	/**
+	 * Closes socket & streams as well as listener thread and then terminates
+	 * program.
+	 */
+	private void killIO() {
+		try {
+			listener.setAlive(false);
+			socket.close();
+			System.out.println("Thank you for using this chattclient \nSuccessful termination");
+			System.exit(0);
+		} catch (IOException e) {
+			System.out.println("Could not close socket properly \nUnsuccessful termination");
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * Main method. takes hostName as 1st argument and portNr as second argument.
+	 * 
+	 * @param args holds arguments from user.
+	 */
+	public static void main(String[] args) {
+		String hostName = args.length > 0 ? args[0] : "localhost";
+		int portNr = args.length > 1 ? Integer.parseInt(args[1]) : 2000;
+
+		new Client(hostName, portNr);
+	}
+}
---
+++ Listener.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ * Listener class of ChatClient.
+ * Extends thread and listens for incoming
+ * data until shut down by main class.
+ * 
+ * @author Ivar Lund
+ * ivlu1468
+ * ivarnilslund@gmail.com
+ * 
+ */
+public class Listener extends Thread {
+
+	private InputStreamReader input;
+	private BufferedReader reader;
+	private final Socket clientSocket;
+	private boolean alive = true;
+
+	/**
+	 * Class constructor. Takes one parameter provided at initialization.
+	 * 
+	 * @param socket the socket to be listened too.
+	 */
+	public Listener(Socket socket) {
+		this.clientSocket = socket;
+		setupListener();
+	}
+	
+	/**
+	 * Worker method of this class. listens to input stream for data and displays
+	 * data to STDOUT. Lifespan determined by while loop.
+	 */
+	public void run() {
+		System.out.println("Input ready!");
+		String msg = null;
+		while (alive) {
+			try {
+				msg = reader.readLine();
+			} catch (IOException e) {
+				System.out.println("Server connection lost");
+				System.exit(1);
+				e.printStackTrace(System.out);
+			}
+			System.out.println(msg);
+		}
+	}
+
+	/**
+	 * Setter for variable determining the lifespan of the method run()
+	 * 
+	 * @param alive pass false to kill thread.
+	 */
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+
+	/**
+	 * Initiates input stream and buffer.
+	 */
+	private void setupListener() {
+		try {
+			input = new InputStreamReader(clientSocket.getInputStream());
+			reader = new BufferedReader(input);
+		} catch (IOException e) {
+			System.out.println("Error: could not set up input.");
+			e.printStackTrace();
+		}
+	}
+
+}
